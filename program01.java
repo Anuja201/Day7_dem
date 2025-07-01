2208c2eb8693a41853ace29346e43c0707ea8029
Anuja201 <anujaherwade31@gmail.com>
2025-06-30 23:40:32 -0700

    dem2

--- program01.java
+++ program01.java
@@ -1,86 +1,12 @@
-package com.dkte.demo1;
+package com.dkte.demo2;
 
-import java.util.Scanner;
 
-interface Acceptable{
-	void accept(Scanner sc);
-}
-
-interface Displayable{
-	void display();
-}
-
-class Date implements Acceptable,Displayable{
-	int day;
-	int month;
-	int year;
-	
-	@Override
-	public void accept(Scanner sc) {
-		System.out.println("Enter the day:");
-		this.day=sc.nextInt();
-		System.out.println("Enter the month:");
-		this.month=sc.nextInt();
-		System.out.println("Enter the year:");
-		this.year=sc.nextInt();
-	}
-	
-	@Override
-	public void display() {
-		System.out.println("Date :: "+day+"/"+month+"/"+year);
-	}
-	
-}
-class Time implements Acceptable{
-	int hr;
-	int min;
-	
-	@Override
-	public void accept(Scanner sc) {
-		System.out.println("Enter the hr:");
-		this.hr=sc.nextInt();
-		System.out.println("Enter the min:");
-		this.min=sc.nextInt();
-	}
-
-}
-class Employee implements Acceptable, Displayable {
-	int id;
-	String name;
-
-	@Override
-	public void accept(Scanner sc) {
-		System.out.println("Enter the ID:");
-		this.id=sc.nextInt();
-		System.out.println("Enter the Name:");
-		this.name=sc.next();
-		sc.nextLine();
-	}
-
-	@Override
-	public void display() {
-		System.out.println("Employee :: Id-"+id+",Name-"+name);
-	}
-
-}
 public class program01 {
-	
+
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		Acceptable a1 = new Date();//upcasting
-		a1.accept(sc);
-		Displayable d1 = (Displayable) a1;//downcasting
-		d1.display();
+		double arr[] = new double[999999999];
+		arr[0] = 10;
 
-		// Time
-		a1 = new Time();
-		a1.accept(sc);
-
-		// Employee
-		Acceptable a2 = new Employee();
-		a2.accept(sc);
-		Displayable d2 = (Displayable) a2;
-		d2.display();
-	
 	}
+
 }
--- program02.java
+++ program02.java
@@ -1,46 +1,16 @@
-package com.dkte.demo1;
+package com.dkte.demo2;
 
-interface I1{
-	
-}
-interface I2{
 
-}
-interface I3 extends I1,I2{
-	
-}
-class c1{
-	
-}
-class c2{
-	
-}
-class c3 extends c1 implements I1,I2{
-	
-}
-
+//Static use of program
 public class program02 {
+	public static void test() {
+		double num1;
+		test();
+	}
 
 	public static void main(String[] args) {
-		// class c3 extends c1   //Ok
-		//class c3 extends c1,c2  //Not Ok
-		//class c3 extends I1     // ok
-		//class c3 extends I1,I2 //Not Ok
-		//class c3 Implements I1//Ok
-		//class c3 implements I1,I2  //Ok
-		
-		//interface I3 implements I1  // Not Ok
-		//interface I3 implements I1,I2 //NOt ok
-		//interface I3 implements c1  //Not ok
-		//interface I3 implements c2,c1 //not ok
-		//interface I3 extends c1 //not ok
-		//interface I3 extends c1,c2 //not ok
-		//interface I3 extends I1 // ok
-		//interface I3 extends I1,I2 // ok
-		
-//		class C3 extends C1 implements I1, I2// OK
-//		class C3 implements I1, I2 extends C1 // NOT OK
-		
+		// TODO Auto-generated method stub
+		test();
 
 	}
 
